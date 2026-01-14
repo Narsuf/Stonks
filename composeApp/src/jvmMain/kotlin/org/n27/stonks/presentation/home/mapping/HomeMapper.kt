883df9393d5a214f3012686baf0eb5bfaf298f2b
Jorge <jorge.dominguez@n26.com>
2026-01-14 16:01:21 +0100

    Create StringResourceWithArgs

--- composeApp/src/jvmMain/kotlin/org/n27/stonks/presentation/home/mapping/HomeMapper.kt
+++ composeApp/src/jvmMain/kotlin/org/n27/stonks/presentation/home/mapping/HomeMapper.kt
@@ -1,10 +1,13 @@
 package org.n27.stonks.presentation.home.mapping
 
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
-import org.jetbrains.compose.resources.getString
 import org.n27.stonks.domain.models.Stocks
 import org.n27.stonks.domain.models.Stocks.Stock
+import org.n27.stonks.presentation.common.entities.StringResourceWithArgs
+import org.n27.stonks.presentation.common.entities.StringResourceWithArgs.Arg.Resource
+import org.n27.stonks.presentation.common.entities.StringResourceWithArgs.Arg.Text
 import org.n27.stonks.presentation.common.extensions.*
 import org.n27.stonks.presentation.home.entities.HomeState.Content
 import org.n27.stonks.presentation.home.entities.HomeState.Content.BottomSheet
@@ -14,7 +17,7 @@ import stonks.composeapp.generated.resources.not_set
 import stonks.composeapp.generated.resources.valuation_and_growth
 import java.math.BigDecimal
 
-internal suspend fun Stocks.toContent() = Content(
+internal fun Stocks.toContent() = Content(
     bottomSheet = BottomSheet(
         epsGrowthInput = BigDecimal.ZERO,
         valuationFloorInput = BigDecimal.ZERO,
@@ -25,18 +28,24 @@ internal suspend fun Stocks.toContent() = Content(
     isPageLoading = false,
 )
 
-internal suspend fun List<Stock>.toPresentationEntity(): ImmutableList<Content.Item> = map { it.toPresentationEntity() }
+internal fun List<Stock>.toPresentationEntity(): ImmutableList<Content.Item> = map { it.toPresentationEntity() }
     .toPersistentList()
 
-private suspend fun Stock.toPresentationEntity() = Content.Item(
+private fun Stock.toPresentationEntity() = Content.Item(
     icon = logo?.toImageBitmap(),
     name = companyName.substringBefore(" "),
     symbol = symbol,
     price = price?.toPrice(currency),
     targetPrice = price?.getTargetPrice(currentIntrinsicValue, currency),
-    extraValue = getString(
-        Res.string.valuation_and_growth,
-        valuationFloor?.toFormattedString() ?: getString(Res.string.default_value),
-        expectedEpsGrowth?.toFormattedPercentage() ?: getString(Res.string.not_set),
+    extraValue = StringResourceWithArgs(
+        resource = Res.string.valuation_and_growth,
+        args = persistentListOf(
+            valuationFloor?.toFormattedString()
+                ?.let { Text(it) }
+                ?: Resource(Res.string.default_value),
+            expectedEpsGrowth?.toFormattedPercentage()
+                ?.let { Text(it) }
+                ?: Resource(Res.string.not_set),
+        ),
     ),
 )
--- composeApp/src/jvmTest/kotlin/org/n27/stonks/test_data/presentation/HomeContentDataGenerator.kt
+++ composeApp/src/jvmTest/kotlin/org/n27/stonks/test_data/presentation/HomeContentDataGenerator.kt
@@ -1,11 +1,16 @@
 package org.n27.stonks.test_data.presentation
 
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
 import org.n27.stonks.presentation.common.composables.DeltaState
 import org.n27.stonks.presentation.common.composables.DeltaTextEntity
+import org.n27.stonks.presentation.common.entities.StringResourceWithArgs
+import org.n27.stonks.presentation.common.entities.StringResourceWithArgs.Arg
 import org.n27.stonks.presentation.home.entities.HomeState.Content
 import org.n27.stonks.presentation.home.entities.HomeState.Content.BottomSheet
 import org.n27.stonks.presentation.home.entities.HomeState.Content.Item
+import stonks.composeapp.generated.resources.Res
+import stonks.composeapp.generated.resources.valuation_and_growth
 import java.math.BigDecimal
 
 internal fun getHomeContent(
@@ -39,7 +44,13 @@ internal fun getHomeItem(
         percentage = "64.00 %",
         state = DeltaState.NEGATIVE
     ),
-    extraValue: String? = "12.50 / 7.72 %",
+    extraValue: StringResourceWithArgs? = StringResourceWithArgs(
+        resource = Res.string.valuation_and_growth,
+        args = persistentListOf<Arg>(
+            Arg.Text("12.50"),
+            Arg.Text("7.72 %"),
+        )
+    ),
 ) = Item(
     icon = null,
     name = name,
