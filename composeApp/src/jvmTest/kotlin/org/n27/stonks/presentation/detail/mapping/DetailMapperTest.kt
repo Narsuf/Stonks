94ef1604238fb0ef6b4a85c26860916b32d6cb95
Narsuf <jorgedguezm@gmail.com>
2026-03-30 10:57:49 +0200

    Test null cells

--- composeApp/src/jvmTest/kotlin/org/n27/stonks/presentation/detail/mapping/DetailMapperTest.kt
+++ composeApp/src/jvmTest/kotlin/org/n27/stonks/presentation/detail/mapping/DetailMapperTest.kt
@@ -3,9 +3,15 @@ package org.n27.stonks.presentation.detail.mapping
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.n27.stonks.presentation.common.AppColors
+import org.n27.stonks.presentation.common.composables.DeltaState
+import org.n27.stonks.presentation.common.composables.DeltaTextEntity
+import org.n27.stonks.presentation.detail.entities.DetailState.Content.Item
 import org.n27.stonks.test_data.domain.getFredYields
 import org.n27.stonks.test_data.domain.getStock
 import org.n27.stonks.test_data.presentation.getDetailContent
+import org.n27.stonks.test_data.presentation.getDetailContentCell
+import stonks.composeapp.generated.resources.*
 import java.util.*
 
 class DetailMapperTest {
@@ -21,5 +27,30 @@ class DetailMapperTest {
         val result = getStock().toDetailContent(fredYields = getFredYields())
 
         assertEquals(getDetailContent(), result)
+    }
+
+    @Test
+    fun `toDetailContent should map Stock to Content with only headers when all optional fields are null`() {
+        val result = getStock(
+            dividends = null,
+            incomeStatement = null,
+            valuationMeasures = null,
+            computed = null,
+            roe = null,
+            profitMargin = null,
+            balanceSheet = null,
+            earningsEstimate = null,
+        ).toDetailContent()
+
+        assertEquals(
+            getDetailContent(
+                items = listOf(
+                    Item.Header(Res.string.section_dividends),
+                    Item.Header(Res.string.section_valuation),
+                    Item.Header(Res.string.section_fundamentals),
+                )
+            ),
+            result,
+        )
     }
 }
